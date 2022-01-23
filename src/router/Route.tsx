4333fa7ef7c8160ae128c46926f51f8ec99a3e12
Tayyab <tayyab.shahid7@gmail.com>
2022-01-24 01:21:02 +0500

    make it better

--- src/router/Route.tsx
+++ src/router/Route.tsx
@@ -3,15 +3,15 @@ import { Router } from 'react-router-dom';
 import PublicRoute from "./PublicRoute";
 import history from "../utils/history";
 import Survey from "../pages/Survey";
-import SurveyDetail from "../pages/SurveyDetail";
+// import SurveyDetail from "../pages/SurveyDetail";
 
 const Route = () => {
 
   return (
     <Router history={history}>
-        <PublicRoute component={Survey} path="/survey" exact />
+        <PublicRoute component={Survey} path="/" exact />
         {/*<PublicRoute component={Survey} path="/" exact />*/}
-        <PublicRoute component={SurveyDetail} path="/survey-detail/:id" exact />
+        {/*<PublicRoute component={SurveyDetail} path="/survey-detail/:id" exact />*/}
     </Router>
   );
 };
